227e28294a61ba54cc6c896cc1c46c0d910d1088
JYOAT <atwaljyotsna@gmail.com>
2025-07-24 20:37:51 +0530

    changes

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -18,41 +18,41 @@ export function getReceiverSocketId(userId) {
 //used to store online users
 const userSocketMap = {}; //{userId: socketId}
 
-// io.on("connection", (socket) => {
-//     console.log("A user connected", socket.id);
-//     const userId = socket.handshake.query.userId;
-//     if (userId) userSocketMap[userId] = socket.id;
-
-//     // io.emit() is used to send events to all the connected clients
-//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-//     socket.on("disconnect", () => {
-//         console.log("A user disconnected", socket.id);
-//         delete userSocketMap[userId];
-//         io.emit("getOnlineUsers", Object.keys(userSocketMap));
-//     })
-// })
-
 io.on("connection", (socket) => {
     console.log("A user connected", socket.id);
+    const userId = socket.handshake.query.userId;
+    if (userId) userSocketMap[userId] = socket.id;
 
-    const rawUserId = socket.handshake.query.userId;
-
-    if (!rawUserId || typeof rawUserId !== "string" || rawUserId.includes(":")) {
-        console.warn("Invalid userId received from socket:", rawUserId);
-        return;
-    }
-
-    const userId = rawUserId;
-    userSocketMap[userId] = socket.id;
-
+    // io.emit() is used to send events to all the connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
     socket.on("disconnect", () => {
         console.log("A user disconnected", socket.id);
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
-});
+    })
+})
+
+// io.on("connection", (socket) => {
+//     console.log("A user connected", socket.id);
+
+//     const rawUserId = socket.handshake.query.userId;
+
+//     if (!rawUserId || typeof rawUserId !== "string" || rawUserId.includes(":")) {
+//         console.warn("Invalid userId received from socket:", rawUserId);
+//         return;
+//     }
+
+//     const userId = rawUserId;
+//     userSocketMap[userId] = socket.id;
+
+//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+//     socket.on("disconnect", () => {
+//         console.log("A user disconnected", socket.id);
+//         delete userSocketMap[userId];
+//         io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//     });
+// });
 
 export { io, app, server };
